8ff0cdacf25e802481e7be09cbdc795c568134ef
Guoguo <i@qwq.trade>
2025-12-09 14:24:36 +0800

    feat: use button instead of slider

--- browser/src/components/menu/video/rotate.tsx
+++ browser/src/components/menu/video/rotate.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
-import { Popover, Slider } from 'antd'
-import { useAtom } from 'jotai'
-import { ScalingIcon } from 'lucide-react'
+import { Popover } from 'antd'
+import { useSetAtom } from 'jotai'
+import { RotateCcwIcon } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
 import { videoRotateAtom } from '@/jotai/device.ts';
@@ -9,8 +9,9 @@ import * as storage from '@/libs/storage';
 
 export const Rotate = (): ReactElement => {
   const { t } = useTranslation()
+  const rotates = [0, 90, 180, 270];
 
-  const [videoRotate, setVideoRotate] = useAtom(videoRotateAtom)
+  const setVideoRotate = useSetAtom(videoRotateAtom)
 
   async function updateRotate(rotate: number): Promise<void> {
     setVideoRotate(rotate)
@@ -18,31 +19,25 @@ export const Rotate = (): ReactElement => {
   }
 
   const content = (
-    <div className="h-[150px] w-[60px] py-3">
-      <Slider
-        vertical
-        marks={{
-          0: <span>0</span>,
-          90: <span>90</span>,
-          180: <span>180</span>,
-          270: <span>270</span>
-        }}
-        range={false}
-        included={false}
-        min={0}
-        max={270}
-        step={90}
-        defaultValue={videoRotate}
-        onChange={updateRotate}
-      />
-    </div>
+    <>
+      {rotates.map((rotate) => (
+        <div
+          key={rotate}
+          className={
+            'flex cursor-pointer select-none items-center space-x-1 rounded px-3 py-1.5 hover:bg-neutral-700/60'}
+          onClick={() => updateRotate(rotate)}
+        >
+          <span className="w-[32px]">{rotate}°</span>
+        </div>
+      ))}
+    </>
   )
 
   return (
     <Popover content={content} placement="rightTop" arrow={false} align={{ offset: [13, 0] }}>
       <div className="flex h-[30px] cursor-pointer items-center space-x-1 rounded px-3 text-neutral-300 hover:bg-neutral-700/50">
         <div className="flex h-[14px] w-[20px] items-end">
-          <ScalingIcon size={16} />
+          <RotateCcwIcon size={16} />
         </div>
         <span>{t('video.rotate')}</span>
       </div>
